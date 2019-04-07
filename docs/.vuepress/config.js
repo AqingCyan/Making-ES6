a4cca65a784b83ab4edd4888a78b8ea93e121f75
AqingCyan <xuekaiqi@creatshare.com>
2019-04-08 00:36:39 +0800

    :ok_hand:基本更新完毕

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,9 +8,9 @@ module.exports = {
     lastUpdated: '上次更新',
     sidebar: [
       ['/','写在文档之前'],
-      ['/ES6book/01_letAndconst','let与const以及块级作用域'],
-      ['/ES6book/02_jiegoufuzhi', '解构赋值'],
-      ['/ES6book/03_strInES6', '字符串的拓展'],
+      ['ES6book/01_letAndconst','let与const以及块级作用域'],
+      ['ES6book/02_jiegoufuzhi', '解构赋值'],
+      ['ES6book/03_strInES6', '字符串的拓展'],
       ['ES6book/04_ArrInES6', '数组的拓展'],
       ['ES6book/05_FunctionInES6', '函数的拓展'],
       ['ES6book/06_ObjectInES6', '对象的拓展'],
@@ -18,7 +18,10 @@ module.exports = {
       ['ES6book/08_setAndmap', 'Set与Map数据类型'],
       ['ES6book/09_proxyAndReflect', 'Proxy代理'],
       ['ES6book/10_class', '用class来定义类'],
-      ['ES6book/11_Promise', 'Promise']
+      ['ES6book/11_Promise', 'Promise'],
+      ['ES6book/12_Iterator', 'Iterator'],
+      ['ES6book/13_Generator', 'Generator'],
+      ['ES6book/14_Decorator', 'Decorator修饰器']
     ]
   },
   base: '/Making-ES6/'
